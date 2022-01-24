41e57ff8f9be18855cdc8681868bd1c99489b5b4
nabat21 <76848446+nabat21@users.noreply.github.com>
2022-01-24 11:48:51 +0330

    Update Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -1,7 +1,14 @@
+x = input("Enter farinhait(1) or cantigrad(2)?")
+
+farinhait = 1
+cantigrad = 2
+
 dama = int(input("Enter your number:"))
 
-farinhait = ((dama * 1.8) + 32)
-cantigrad = ((dama - 32)/ 1.8)
+if x == '1':
+    farinhait = ((dama * 1.8) + 32)
+    print("F:", farinhait)
 
-print("F:", farinhait)
-print("C:", cantigrad)
+elif x == '2':
+    cantigrad = ((dama - 32)/ 1.8)
+    print("C:", cantigrad)
